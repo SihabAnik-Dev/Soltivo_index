7a5f725d7dd797f01bec1ef619204506e302a588
SihabAnik-Dev <sihabanik1@gmail.com>
2023-03-18 01:00:01 +0600

    update korci

--- js/cunter.js
+++ js/cunter.js
@@ -2,7 +2,45 @@ $('.banner_slide').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
-autoplaySpeed: 1000,
-nextArrow:".next",
-prevArrow:".prev",
+  autoplaySpeed: 1000,
+  nextArrow:".next",
+  prevArrow:".prev",
 });
+VenoBox({
+  selector: '.venobox',
+});
+
+$(document).ready(function(){
+  $('.port_main').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    slideToScroll:1,
+    centerPadding:"0px",
+    nextArrow:".port-next",
+    prevArrow:".port-prev",
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+})
+
+
+	
